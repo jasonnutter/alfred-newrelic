3327dc7203074d79b0ff1eb23469be485d401b64
Jason Nutter <jasondnutter@gmail.com>
2018-11-08 18:24:22 -0800

    Fix lint

--- src/index.js
+++ src/index.js
@@ -5,37 +5,37 @@ const apiKey = alfy.config.get('API_KEY');
 const accountId = alfy.config.get('ACCOUNT_ID');
 
 if (apiKey && accountId) {
-	Promise.all([
-		alfy.fetch(`https://api.newrelic.com/v2/applications.json?filter[name]=${input}`, {
-			headers: {
-				'X-Api-Key': apiKey
-			},
-			json: true
-		}),
-		alfy.fetch(`https://api.newrelic.com/v2/browser_applications.json?filter[name]=${input}`, {
-			headers: {
-				'X-Api-Key': apiKey
-			},
-			json: true
-		})
-	])
+    Promise.all([
+        alfy.fetch(`https://api.newrelic.com/v2/applications.json?filter[name]=${input}`, {
+            headers: {
+                'X-Api-Key': apiKey
+            },
+            json: true
+        }),
+        alfy.fetch(`https://api.newrelic.com/v2/browser_applications.json?filter[name]=${input}`, {
+            headers: {
+                'X-Api-Key': apiKey
+            },
+            json: true
+        })
+    ])
         .then(([ apm, browser ]) => {
             const apmApps = apm.applications.map(app => ({
                 title: `${app.name} (APM)`,
                 subtitle: `ID: ${app.id} | Language: ${app.language}`,
                 arg: `https://rpm.newrelic.com/accounts/${accountId}/applications/${app.id}`
-			}));
+            }));
 
             const browserApps = browser.browser_applications.map(app => ({
                 title: `${app.name} (Browser)`,
                 subtitle: `ID: ${app.id}`,
                 arg: `https://rpm.newrelic.com/accounts/${accountId}/browser/${app.id}`
-			}));
+            }));
 
             alfy.output([
-				...apmApps,
-				...browserApps
-			]);
+                ...apmApps,
+                ...browserApps
+            ]);
         });
 } else {
     const errors = [];
